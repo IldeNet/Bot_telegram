e7c9064b66d78c4cc78a3427eb0d3527d69fdccb
IldeNet <ildenet@hotmail.com>
2016-09-19 14:40:04 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+require 'vendor/autoload.php';
+
+$client = Zelenin\Telegram\Bot\ApiFactory::create('here you token of botfather'); // Set your access token
+$update = json_decode(file_get_contents('php://input'));
+
+//your app
+try {
+
+    if($update->message->text == 'hello')
+    {
+    	$response = $client->sendMessage([
+        	'chat_id' => $update->message->chat->id,
+        	'text' => "Hello, this working!"
+     	]);
+    }
+    else 
+    {
+    }
+} catch (\Zelenin\Telegram\Bot\NotOkException $e) {
+
+}
